3b7c6e865a9dd72fb7541684d4fd75f680b382be
Tihomir Petrov (PC Privat) <nv@netzlab.de>
2021-10-05 10:51:28 +0200

    sync

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/test', function () {
+   echo 'test';
+});
+Route::get('/category', function () {
+    return view('wip.category.form');
+});
