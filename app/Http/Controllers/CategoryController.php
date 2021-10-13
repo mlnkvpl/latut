f36f02ce9e4e53b684da81e054070279285f546e
Tihomir Petrov (PC Privat) <nv@netzlab.de>
2021-10-13 08:52:43 +0200

    update CategoryController method delete

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -83,6 +83,7 @@ class CategoryController extends Controller
         $category = Category::find($id);
         $category->delete();
 
-        return $this->view();
+        // return $this->view();
+        return back();
     }
 }
